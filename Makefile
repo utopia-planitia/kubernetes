5db0166bd177dadab45dae9486577bc74fca1e23
David Sauer <davedamoon@gmail.com>
2017-08-20 18:58:42 +0200

    provision and destroy nodes at ovh

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 
 include ./etc/help.mk
 include ./etc/cli.mk
+include ./etc/ovh.mk
 include ./etc/vagrant/vagrant.mk
+
+#ANSIBLE_OPTIONS ?= "-vvvv"
 
 .PHONY: system-requirements-check
 system-requirements-check: ##@setup checks system for required dependencies
-	@./etc/system-requirements-check.sh
+	./etc/system-requirements-check.sh
 
 .PHONY: deploy
 deploy: ##@ansible deploy
-	@ansible-playbook playbook.yml ${OPTIONS}
+	$(CLI) ansible-playbook deploy.yml ${ANSIBLE_OPTIONS}
--- etc/cli.mk
+++ etc/cli.mk
@@ -3,7 +3,7 @@ DEV_TOOLS_DOCKER_IMAGE_PREBUILD ?= davedamoon/kubernetes-cluster-dev-tools
 DEV_TOOLS_DOCKER_IMAGE_LOCAL ?= kubernetes-cluster-dev-tools
 DEV_TOOLS_DOCKER_IMAGE_EXISTS = $(shell which docker && docker images -q ${DEV_TOOLS_DOCKER_IMAGE_LOCAL})
 DEV_TOOLS_DOCKER_IMAGE = $(shell [ "${DEV_TOOLS_DOCKER_IMAGE_EXISTS}" ] && echo ${DEV_TOOLS_DOCKER_IMAGE_LOCAL} || echo ${DEV_TOOLS_DOCKER_IMAGE_PREBUILD})
-CLI_HOST = docker run --net=host --rm -ti -v $(PWD):/workspace -w /workspace ${DEV_TOOLS_DOCKER_IMAGE}
+CLI_HOST = docker run --net=host --rm -ti -v $(PWD):/workspace -v ~/.ssh:/root/.ssh -v ~/.vagrant.d/:/root/.vagrant.d/ -w /workspace ${DEV_TOOLS_DOCKER_IMAGE}
 CLI_DOCKER = ""
 CLI = $(shell [ "$(shell cat /proc/1/cgroup | grep docker)" ] && echo ${CLI_DOCKER} || echo ${CLI_HOST})
 
