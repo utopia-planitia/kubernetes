fc5915c40884718d38f4b3b3d462afceaf7895d6
David Sauer <davedamoon@gmail.com>
2017-08-21 21:38:33 +0200

    moved project to gitlab.com

--- etc/cli.mk
+++ etc/cli.mk
@@ -1,19 +1,20 @@
 
-DEV_TOOLS_DOCKER_IMAGE_PREBUILD ?= davedamoon/kubernetes-cluster-dev-tools
-DEV_TOOLS_DOCKER_IMAGE_LOCAL ?= kubernetes-cluster-dev-tools
-DEV_TOOLS_DOCKER_IMAGE_EXISTS = $(shell which docker && docker images -q ${DEV_TOOLS_DOCKER_IMAGE_LOCAL})
-DEV_TOOLS_DOCKER_IMAGE = $(shell [ "${DEV_TOOLS_DOCKER_IMAGE_EXISTS}" ] && echo ${DEV_TOOLS_DOCKER_IMAGE_LOCAL} || echo ${DEV_TOOLS_DOCKER_IMAGE_PREBUILD})
+DEV_TOOLS_DOCKER_IMAGE ?= registry.gitlab.com/davedamoon/kubernetes-via-ansible
 CLI_HOST = docker run --net=host --rm -ti -v $(PWD):/workspace -v ~/.ssh:/root/.ssh -v ~/.vagrant.d/:/root/.vagrant.d/ -w /workspace ${DEV_TOOLS_DOCKER_IMAGE}
 CLI_DOCKER = ""
 CLI = $(shell [ "$(shell cat /proc/1/cgroup | grep docker)" ] && echo ${CLI_DOCKER} || echo ${CLI_HOST})
 
 .PHONY: build-cli-image
-build-cli-image: ##@development creates command line interface
-	docker build --pull -t ${DEV_TOOLS_DOCKER_IMAGE_LOCAL} ./docker
+build-cli-image: ##@development build DEV_TOOLS_DOCKER_IMAGE from docker directory
+	docker build --pull -t ${DEV_TOOLS_DOCKER_IMAGE} ./docker
+
+.PHONY: pull-cli-image
+pull-cli-image: ##@development pull latest version of DEV_TOOLS_DOCKER_IMAGE
+	docker pull ${DEV_TOOLS_DOCKER_IMAGE}
 
 .PHONY: delete-cli-image
-delete-cli-image: ##@development removes the local cli image
-	docker rmi ${DEV_TOOLS_DOCKER_IMAGE_LOCAL}
+delete-cli-image: ##@development removes the local DEV_TOOLS_DOCKER_IMAGE
+	docker rmi ${DEV_TOOLS_DOCKER_IMAGE}
 
 .PHONY: cli
 cli: ##@development creates command line interface
