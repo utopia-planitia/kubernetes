2c476b1a53262cbd53ff8b49eb4895399504f195
David Sauer <davedamoon@gmail.com>
2017-08-23 22:27:23 +0200

    fixed openstack provisioning

--- etc/ovh.mk
+++ etc/ovh.mk
@@ -1,9 +1,10 @@
-export CURRENT_GIT_BRANCH = $(shell git rev-parse --abbrev-ref HEAD)
+CURRENT_GIT_BRANCH = $(shell git rev-parse --abbrev-ref HEAD)
+OVH_KEYPAIR_NAME = $(shell hostname | base64 -w 0)
 
 .PHONY: ovh-provision
 ovh-provision: ##@ovh start vms and create inventory
-	$(CLI) ansible-playbook ovh-provision.yml -e "OVH_NODES_PREFIX=$(CURRENT_GIT_BRANCH)" ${ANSIBLE_OPTIONS}
+	$(CLI) ansible-playbook ovh-provision.yml -e "OVH_NODES_PREFIX=$(CURRENT_GIT_BRANCH) OVH_KEYPAIR_NAME=$(OVH_KEYPAIR_NAME)" ${ANSIBLE_OPTIONS}
 
 .PHONY: ovh-destroy
 ovh-destroy: ##@ovh stop and remove vms
-	$(CLI) ansible-playbook ovh-destroy.yml -e "OVH_NODES_PREFIX=$(CURRENT_GIT_BRANCH)" ${ANSIBLE_OPTIONS}
+	$(CLI) ansible-playbook ovh-destroy.yml -e "OVH_NODES_PREFIX=$(CURRENT_GIT_BRANCH) OVH_KEYPAIR_NAME=$(OVH_KEYPAIR_NAME)" ${ANSIBLE_OPTIONS}
