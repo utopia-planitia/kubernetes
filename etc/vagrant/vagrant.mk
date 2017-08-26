28af9c2030badaceb26cd326dee678b642fd0187
David Sauer <davedamoon@gmail.com>
2017-08-26 14:39:50 +0200

    mark install python only as changed when fresh installed
    fix #5

--- etc/vagrant/vagrant.mk
+++ etc/vagrant/vagrant.mk
@@ -19,4 +19,5 @@ vagrant-destroy: ##@vagrant stop and remove vms
 .PHONY: vagrant-provision
 vagrant-provision: ##@vagrant start vms and create inventory
 	$(MAKE) vagrant-start
+	unlink ./inventory
 	cp ./etc/vagrant/inventory-${VAGRANT_OS} ./inventory
