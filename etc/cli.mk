b6597598626cd1d01448422268061092e35fe720
David Sauer <davedamoon@gmail.com>
2017-09-05 08:24:50 +0200

    use versioned docker images

--- etc/cli.mk
+++ etc/cli.mk
@@ -1,20 +1,12 @@
 
-DEV_TOOLS_DOCKER_IMAGE ?= registry.gitlab.com/davedamoon/kubernetes-via-ansible
+DEV_TOOLS_DOCKER_IMAGE ?= registry.gitlab.com/davedamoon/kubernetes-the-bare-metal-way-tools:v1
 CLI_HOST = docker run --net=host --rm -ti -v $(PWD):/workspace -v ~/.ssh/ovh:/root/.ssh -v ~/.vagrant.d/:/root/.vagrant.d/ -w /workspace -e KUBECONFIG=/workspace/certificates/master/admin-kube-config ${DEV_TOOLS_DOCKER_IMAGE}
 CLI_DOCKER = ""
 CLI = $(shell [ "$(shell cat /proc/1/cgroup | grep docker)" ] && echo ${CLI_DOCKER} || echo ${CLI_HOST})
 
-.PHONY: build-cli-image
-build-cli-image: ##@development build DEV_TOOLS_DOCKER_IMAGE from docker directory
-	docker build --pull -t ${DEV_TOOLS_DOCKER_IMAGE} ./docker
-
-.PHONY: pull-cli-image
-pull-cli-image: ##@development pull latest version of DEV_TOOLS_DOCKER_IMAGE
+.PHONY: cli-pull-image
+cli-pull-image: ##@development pull latest version of DEV_TOOLS_DOCKER_IMAGE
 	docker pull ${DEV_TOOLS_DOCKER_IMAGE}
-
-.PHONY: delete-cli-image
-delete-cli-image: ##@development removes the local DEV_TOOLS_DOCKER_IMAGE
-	docker rmi ${DEV_TOOLS_DOCKER_IMAGE}
 
 .PHONY: cli
 cli: ##@development creates command line interface
