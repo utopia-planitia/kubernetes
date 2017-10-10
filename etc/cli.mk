0c00d4c66abbc829b9f675cd01ad4fff6558f285
David Sauer <davedamoon@gmail.com>
2017-10-10 21:45:40 +0200

    moved project

--- etc/cli.mk
+++ etc/cli.mk
@@ -1,5 +1,5 @@
 
-DEV_TOOLS_DOCKER_IMAGE ?= registry.gitlab.com/davedamoon/kubernetes-the-bare-metal-way-tools:v3
+DEV_TOOLS_DOCKER_IMAGE ?= registry.gitlab.com/utopia-planitia/kubernetes-the-bare-metal-way-tools:v3
 CLI_HOST = docker run --net=host --rm -ti -v $(PWD):/workspace -v ~/.ssh/ovh:/root/.ssh -v ~/.vagrant.d/:/root/.vagrant.d/ -w /workspace -e KUBECONFIG=/workspace/certificates/master/admin-kube-config ${DEV_TOOLS_DOCKER_IMAGE}
 CLI_DOCKER = ""
 CLI = $(shell [ "$(shell cat /proc/1/cgroup | grep docker)" ] && echo ${CLI_DOCKER} || echo ${CLI_HOST})
