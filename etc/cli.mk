1553e3a75f19b9fdcd34045561507ec8063401d0
David Sauer <davedamoon@gmail.com>
2017-10-24 21:37:22 +0200

    use dynamic path to define docker image name

--- etc/cli.mk
+++ etc/cli.mk
@@ -1,5 +1,6 @@
 
-DEV_TOOLS_DOCKER_IMAGE ?= registry.gitlab.com/utopia-planitia/kubernetes-the-bare-metal-way-tools:v3
+IMAGE_TAG ?= v3
+DEV_TOOLS_DOCKER_IMAGE ?= registry.gitlab.com/$(shell basename $(shell dirname $(PWD)))/kubernetes-the-customizable-way-tools:${IMAGE_TAG}
 CLI_HOST = docker run --net=host --rm -ti -v $(PWD):/workspace -v ~/.ssh/ovh:/root/.ssh -v ~/.vagrant.d/:/root/.vagrant.d/ -w /workspace -e KUBECONFIG=/workspace/certificates/master/admin-kube-config ${DEV_TOOLS_DOCKER_IMAGE}
 CLI_DOCKER = ""
 CLI = $(shell [ "$(shell cat /proc/1/cgroup | grep docker)" ] && echo ${CLI_DOCKER} || echo ${CLI_HOST})
