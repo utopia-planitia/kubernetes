b0f13c7514be477e84e5e250d42a9c70b5a13f4e
David Sauer <davedamoon@gmail.com>
2017-11-01 21:30:43 +0100

    renamed make targets to fix listing via help

--- etc/tests.mk
+++ etc/tests.mk
@@ -6,18 +6,18 @@ lint: ##@ansible lint ansible config
 .PHONY: tests
 tests: ##@development run all tests
 	$(MAKE) bats
-	$(MAKE) e2e-conformance
-	$(MAKE) e2e-port-forward
+	$(MAKE) conformance
+	$(MAKE) port-forward
 
 .PHONY: bats
 bats: ##@development run bats tests
 	$(CLI) bats tests/
 
-.PHONY: e2e-conformance
-e2e-conformance: ##@development run e2e-conformance tests
+.PHONY: conformance
+conformance: ##@development run conformance tests
 	$(CLI) sh -c 'cd /go/src/k8s.io/kubernetes && go run hack/e2e.go -get=false -- -v -test -check-version-skew=false --provider=skeleton -test_args="--ginkgo.focus=\[Conformance\] --ginkgo.skip=\[Serial\]|\[Flaky\]|\[Feature:.+\]" --ginkgo-parallel'
 	$(CLI) sh -c 'cd /go/src/k8s.io/kubernetes && go run hack/e2e.go -get=false -- -v -test -check-version-skew=false --provider=skeleton -test_args="--ginkgo.focus=\[Serial\].*\[Conformance\]"'
 
-.PHONY: e2e-port-forward
-e2e-port-forward: ##@development run e2e-port-forward test
+.PHONY: port-forward
+port-forward: ##@development run port-forward test
 	$(CLI) sh -c 'cd /go/src/k8s.io/kubernetes && go run hack/e2e.go -get=false -- -v -test -check-version-skew=false --provider=skeleton -test_args="--ginkgo.focus=port-forward"'
