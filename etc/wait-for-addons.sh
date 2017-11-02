e5cdee9d04237cf1fc4011fd04d75cd838e18216
David Sauer <davedamoon@gmail.com>
2017-11-02 08:01:34 +0100

    fixed wait for addons script

--- etc/wait-for-addons.sh
+++ etc/wait-for-addons.sh
@@ -7,7 +7,7 @@ KUBE_DNS_PODS=2
 
 for ID in $(seq 1 ${WEAVE_PODS}); do
   echo waiting for ${ID}/${WEAVE_PODS} weave pods
-  until [[ $( kubectl -n kube-system get po -l name=weave-net --no-headers=true | grep Running | grep 2/2 | wc -l ) -ge "${POD}" ]]; do
+  until [[ $( kubectl -n kube-system get po -l name=weave-net --no-headers=true | grep Running | grep 2/2 | wc -l ) -ge "${ID}" ]]; do
    sleep 0.5
    echo -n .
   done
@@ -16,7 +16,7 @@ done
 
 for ID in $(seq 1 ${KUBE_DNS_PODS}); do
   echo waiting for ${ID}/${KUBE_DNS_PODS} kube-dns pods
-  until [[ $( kubectl -n kube-system get po -l k8s-app=kube-dns --no-headers=true | grep Running | grep 3/3 | wc -l ) -ge "${POD}" ]]; do
+  until [[ $( kubectl -n kube-system get po -l k8s-app=kube-dns --no-headers=true | grep Running | grep 3/3 | wc -l ) -ge "${ID}" ]]; do
    sleep 0.5
    echo -n .
   done
