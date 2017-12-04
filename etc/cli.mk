3f56804c08d1930237e37eaa51d0fe177c39b331
David Sauer <david.sauer@turbinekreuzberg.com>
2017-12-04 14:08:09 +0100

    mounted host SSH socket into cli environment

--- etc/cli.mk
+++ etc/cli.mk
@@ -1,7 +1,13 @@
 
 IMAGE_TAG ?= v5
 DEV_TOOLS_DOCKER_IMAGE ?= registry.gitlab.com/$(shell basename $(shell dirname $(PWD)))/kubernetes-tools:${IMAGE_TAG}
-CLI_HOST = docker run --net=host --rm -ti -v $(PWD):/workspace -v ~/.ssh/ovh:/root/.ssh -v ~/.vagrant.d/:/root/.vagrant.d/ -w /workspace -e KUBECONFIG=/workspace/certificates/master/admin-kube-config ${DEV_TOOLS_DOCKER_IMAGE}
+DOCKER = docker
+DOCKER_OPTIONS += -v $(PWD):/workspace -w /workspace
+DOCKER_OPTIONS += $(shell [ ! -z "$(SSH_AUTH_SOCK)" ] && echo -v $(SSH_AUTH_SOCK):$(SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$(SSH_AUTH_SOCK))
+DOCKER_OPTIONS += -v ~/.ssh/ovh:/root/.ssh/ovh
+DOCKER_OPTIONS += -v ~/.vagrant.d/:/root/.vagrant.d/
+DOCKER_OPTIONS += -e KUBECONFIG=/workspace/certificates/master/admin-kube-config
+CLI_HOST = $(DOCKER) run --net=host --rm -ti $(DOCKER_OPTIONS) $(DEV_TOOLS_DOCKER_IMAGE)
 CLI_DOCKER = ""
 CLI = $(shell [ "$(shell cat /proc/1/cgroup | grep docker)" ] && echo ${CLI_DOCKER} || echo ${CLI_HOST})
 
