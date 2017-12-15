0f56406144448be5923b307f9d71d9e8cb8815aa
David Sauer <david.sauer@turbinekreuzberg.com>
2017-12-15 11:35:26 +0100

    moved dev tools image into kubernetes repo

--- etc/cli.mk
+++ etc/cli.mk
@@ -1,4 +1,24 @@
 
+
+CLI =
+ifeq ($(shell cat /proc/1/cgroup | grep docker | wc -l), 0)
+  KUBERNETES_TOOLS_IMAGE = $(shell docker build -q ../kubernetes/dev-tools)
+  KUBERNETES_CERTIFICATES ?= $(shell realpath ../kubernetes/certificates)
+  KUBERNETES_ETC ?= $(shell realpath ../kubernetes/etc)
+  DOCKER = docker
+  DOCKER_OPTIONS += -v $(PWD):/workspace -w /workspace
+  DOCKER_OPTIONS += $(shell [ ! -z "$(SSH_AUTH_SOCK)" ] && echo -v $(SSH_AUTH_SOCK):$(SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$(SSH_AUTH_SOCK))
+  DOCKER_OPTIONS += -v ~/.ssh/ovh:/root/.ssh/ovh
+  DOCKER_OPTIONS += -v ~/.vagrant.d/:/root/.vagrant.d/
+  DOCKER_OPTIONS += -v $(KUBERNETES_CERTIFICATES):/kubernetes/certificates -e KUBECONFIG=/kubernetes/certificates/master/admin-kube-config
+  DOCKER_OPTIONS += -v $(KUBERNETES_ETC):/kubernetes/etc
+  CLI = $(DOCKER) run --net=host --rm -ti $(DOCKER_OPTIONS) $(KUBERNETES_TOOLS_IMAGE)
+endif
+
+.PHONY: cli
+cli: ##@development creates admin command line interface
+	$(CLI)
+
 .PHONY: demo-cli
 demo-cli: ##@development creates user command line interface
-	$(CLI) sh -c "KUBECONFIG=/workspace/certificates/master/demo-user-kube-config bash"
+	$(CLI) sh -c "KUBECONFIG=/workspace/certificates/user/demo-user-kube-config bash"
