bb4c112c59c6c9ce5392dd9359508dc4993c31c1
David Sauer <david.sauer@turbinekreuzberg.com>
2018-05-08 10:45:24 +0200

    fixed flake warning of deprecations

--- tests/deprecated/grep-logs.sh
+++ tests/deprecated/grep-logs.sh
@@ -6,7 +6,7 @@ EXIT=0
 
 while read -r POD; do
     while read -r CONTAINER; do
-        DEPRECATIONS=$(kubectl -n kube-system logs "$POD" -c "$CONTAINER" | grep -i deprecated)
+        DEPRECATIONS=$(kubectl -n kube-system logs "$POD" -c "$CONTAINER" | grep -i deprecated | grep -v "the object has been modified; please apply your changes to the latest version and try again")
         if [ "$DEPRECATIONS" != "" ]; then
             EXIT=1
             echo "found deprecations in container $CONTAINER of pod $POD"
