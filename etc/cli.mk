6afd3effb473d8fd6853f5bcd2f95ca61ee947d9
David Sauer <david.sauer@turbinekreuzberg.com>
2018-08-14 15:14:15 +0200

    include full ~/.ssh folder for mac, because SSH_AUTH_SOCK is broken

--- etc/cli.mk
+++ etc/cli.mk
@@ -17,21 +17,30 @@ ifndef IS_CONTAINERIZED
   KUBERNETES_CERTIFICATES ?= $(shell realpath ../kubernetes/certificates)
   KUBERNETES_ETC ?= $(shell realpath ../kubernetes/etc)
   KUBECONFIG ?= /kubernetes/certificates/master/admin-kube-config
+
   DOCKER ?= docker
   DOCKER_OPTIONS += -v $(PWD):/workspace -w /workspace
-  DOCKER_OPTIONS += $(shell [ ! -z "$(SSH_AUTH_SOCK)" ] && echo -v $(SSH_AUTH_SOCK):$(SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$(SSH_AUTH_SOCK))
-  DOCKER_OPTIONS += -v ~/.ssh/ovh:/root/.ssh/ovh
-  DOCKER_OPTIONS += -v ~/.ssh/digital-ocean:/root/.ssh/digital-ocean
+
+  ifneq ($(shell uname), Darwin)
+    DOCKER_OPTIONS += $(shell [ ! -z "$(SSH_AUTH_SOCK)" ] && echo -v $(SSH_AUTH_SOCK):$(SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$(SSH_AUTH_SOCK))
+    DOCKER_OPTIONS += -v ~/.ssh/ovh:/root/.ssh/ovh
+    DOCKER_OPTIONS += -v ~/.ssh/digital-ocean:/root/.ssh/digital-ocean
+  else
+    DOCKER_OPTIONS += -v ~/.ssh:/root/.ssh
+  endif
+
   DOCKER_OPTIONS += -v ~/.vagrant.d/:/root/.vagrant.d/
   DOCKER_OPTIONS += -v $(KUBERNETES_CERTIFICATES):/kubernetes/certificates
   DOCKER_OPTIONS += -e KUBECONFIG=$(KUBECONFIG)
   DOCKER_OPTIONS += -v $(KUBERNETES_ETC):/kubernetes/etc
+
   ifdef NEED_ANSIBLE
     DOCKER_OPTIONS += -v $(shell realpath ../kubernetes)/ansible.cfg:/workspace/ansible.cfg
     DOCKER_OPTIONS += -v $(shell realpath ../kubernetes)/inventory:/workspace/inventory
     DOCKER_OPTIONS += -v $(shell realpath ../kubernetes)/group_vars:/workspace/group_vars
     DOCKER_OPTIONS += -v $(shell realpath ../kubernetes)/host_vars:/workspace/host_vars
   endif
+
   CLI = $(DOCKER) run --net=host --rm -ti $(DOCKER_OPTIONS) $(KUBERNETES_TOOLS_IMAGE)
   E2E = $(DOCKER) run --net=host --rm -ti $(DOCKER_OPTIONS) $(KUBERNETES_E2E_IMAGE)
 endif
