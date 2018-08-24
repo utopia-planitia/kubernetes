ab865ed1170e01a16010bffe2c70465325d8636b
David Sauer <david.sauer@turbinekreuzberg.com>
2018-08-24 09:41:42 +0200

    imporved output during 'wait for nodes to start'

--- etc/wait-for-nodes.sh
+++ etc/wait-for-nodes.sh
@@ -6,8 +6,8 @@ NODES_COUNT=$( grep ansible_host inventory | wc -l)
 
 for ID in $(seq 1 $NODES_COUNT); do
   echo waiting for ${ID}/${NODES_COUNT} node registrations
-  until [[ $( kubectl get no --no-headers=true | grep Ready | wc -l ) -ge "${ID}" ]]; do
-   sleep 0.5
+  until [[ $( kubectl get no --no-headers=true 2>&1 | grep Ready | wc -l ) -ge "${ID}" ]]; do
+   sleep 2
    echo -n .
   done
   echo done
