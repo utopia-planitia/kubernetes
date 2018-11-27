c9572363fdbcfe7260caa719097bc09ac243b8a0
David Sauer <david.sauer@turbinekreuzberg.com>
2018-11-27 16:58:44 +0100

    reset e2e namespaces before each test

--- etc/delete-e2e-namespaces.sh
+++ etc/delete-e2e-namespaces.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-until [[ $( kubectl get ns -o=name | grep namespaces/e2e | wc -l ) -eq "0" ]]; do
+until [[ $( kubectl get ns -o=name | grep namespace/e2e | wc -l ) -eq "0" ]]; do
   echo deleting namespaces
-  kubectl get ns -o=name | grep namespaces/e2e | xargs -r -n1 kubectl delete
+  kubectl get ns -o=name | grep namespace/e2e | xargs -r -n1 kubectl delete
   sleep 0.5
   until [[ $( kubectl get ns | grep Terminating | wc -l ) -eq "0" ]]; do
-    echo $( kubectl get ns -o=name | grep namespaces/e2e | wc -l ) namespaces left
+    echo $( kubectl get ns -o=name | grep namespace/e2e | wc -l ) namespaces left
     sleep 0.5
   done
 done
