b0d7018a8331ba1d08531544f72612a3bc1d3d0c
David Sauer <david.sauer@turbinekreuzberg.com>
2018-11-28 17:26:16 +0100

    fixed vagrant provision in case kubernetes is part of a customized repo structure

--- etc/vagrant/vagrant.mk
+++ etc/vagrant/vagrant.mk
@@ -19,5 +19,4 @@ vagrant-destroy: ##@vagrant stop and remove vms
 .PHONY: vagrant-provision
 vagrant-provision: ##@vagrant start vms and create inventory
 	$(MAKE) vagrant-start
-	$(CLI) sh -c 'touch inventory && rm -r inventory'
-	cp ./etc/vagrant/inventory-${VAGRANT_OS} ./inventory
+	$(CLI) sh -c 'cat ./etc/vagrant/inventory-${VAGRANT_OS} > ./inventory'
