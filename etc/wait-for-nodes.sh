3a81818014d1ce9ee73d3a30671cc821e2905343
David Sauer <davedamoon@gmail.com>
2019-02-07 23:43:55 +0100

    deleted kube proxy

--- etc/wait-for-nodes.sh
+++ etc/wait-for-nodes.sh
@@ -12,12 +12,3 @@ for ID in $(seq 1 $NODES_COUNT); do
   done
   echo done
 done
-
-for ID in $(seq 1 $NODES_COUNT); do
-  echo waiting for ${ID}/${NODES_COUNT} kube-proxy
-  until [[ $( kubectl -n kube-system get po -l name=kube-proxy --no-headers=true 2>&1 | grep Running | grep 1/1  | wc -l ) -ge "${ID}" ]]; do
-   sleep 0.5
-   echo -n .
-  done
-  echo done
-done
