6c3eb95b0015abfba6c6ca3f1e7bf6a6c8fcd00a
David Sauer <david.sauer@turbinekreuzberg.com>
2019-06-21 14:46:23 +0200

    disabled npc / updated weave to 2.5.2

--- etc/wait-for-addons.sh
+++ etc/wait-for-addons.sh
@@ -7,7 +7,7 @@ CORE_DNS_PODS=$( grep ansible_host inventory | wc -l)
 
 for ID in $(seq 1 ${NODES_COUNT}); do
   echo waiting for ${ID}/${NODES_COUNT} weave pods
-  until [[ $( kubectl -n kube-system get po -l name=weave-net --no-headers=true | grep Running | grep 2/2 | wc -l ) -ge "${ID}" ]]; do
+  until [[ $( kubectl -n kube-system get po -l name=weave-net --no-headers=true | grep Running | grep 1/1 | wc -l ) -ge "${ID}" ]]; do
    sleep 0.5
    echo -n .
   done
