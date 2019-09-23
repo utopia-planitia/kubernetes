f8bee94243cfa203c344b611687953e8458773a5
David Sauer <david.sauer@turbinekreuzberg.com>
2019-09-23 15:28:11 +0200

    skip kubernetes tests by default, k8s e2e tests fail with RBAC enabled

--- etc/tests.mk
+++ etc/tests.mk
@@ -4,32 +4,35 @@ lint: ##@ansible lint ansible config
 	$(CLI) ansible-lint *.yml roles
 
 .PHONY: tests
-tests: ##@development run all tests
+tests: ##@tests run bats tests
 	make bats
+
+.PHONY: bats
+bats: ##@tests run bats tests
+	$(CLI) bats tests/
+
+.PHONY: kube-tests
+kube-tests: ##@tests run kubernetes e2e tests
 	make conformance
 	make port-forward
 	make serial
 
-.PHONY: bats
-bats: ##@development run bats tests
-	$(CLI) bats tests/
-
 .PHONY: conformance
-conformance: ##@development run conformance tests
+conformance: ##@tests run conformance tests
 	$(E2E) sh -c 'E2E_PARALLEL=y E2E_SKIP="Alpha|Kubectl|Serial|\[(Disruptive|Feature:[^\]]+|Flaky)\]" E2E_FOCUS="\[Conformance\]" /run_e2e.sh'
 
 .PHONY: port-forward
-port-forward: ##@development run port-forward test
+port-forward: ##@tests run port-forward test
 	$(E2E) sh -c 'E2E_PARALLEL=y E2E_SKIP="Alpha|\[(Disruptive|Feature:[^\]]+|Flaky)\]" E2E_FOCUS="port-forward" /run_e2e.sh'
 
 .PHONY: serial
-serial: ##@development run serial test
+serial: ##@tests run serial test
 	$(E2E) sh -c 'E2E_PARALLEL=n E2E_SKIP="Alpha|Kubectl|\[(Disruptive|Feature:[^\]]+|Flaky)\]" E2E_FOCUS="Serial.*Conformance" /run_e2e.sh'
 
 .PHONY: delete-e2e-namespaces
-delete-e2e-namespaces: ##@development remove namespaces created by failed e2e tests
+delete-e2e-namespaces: ##@tests remove namespaces created by failed e2e tests
 	$(CLI) ./etc/delete-e2e-namespaces.sh
 
 PHONY: benchmark-nodes
-benchmark-nodes: ##@development ensure test vms provide enough performance
+benchmark-nodes: ##@tests ensure test vms provide enough performance
 	$(CLI) ansible-playbook prepare-benchmarks.yml ${ANSIBLE_OPTIONS}
