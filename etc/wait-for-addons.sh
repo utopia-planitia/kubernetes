f23530f2b785e10e306934e6ce0ba6dcd32ec584
David Sauer <david.sauer@turbinekreuzberg.com>
2019-11-20 10:24:03 +0100

    cleaned up script output

--- etc/wait-for-addons.sh
+++ etc/wait-for-addons.sh
@@ -6,7 +6,7 @@ NODES_COUNT=$( grep ansible_host inventory | wc -l)
 
 for ID in $(seq 1 ${NODES_COUNT}); do
   echo waiting for ${ID}/${NODES_COUNT} weave pods
-  until [[ $( kubectl -n kube-system get po -l name=weave-net --no-headers=true | grep Running | grep 1/1 | wc -l ) -ge "${ID}" ]]; do
+  until [[ $( kubectl -n kube-system get po -l name=weave-net --no-headers=true --ignore-not-found=true | grep Running | grep 1/1 | wc -l ) -ge "${ID}" ]]; do
    sleep 0.5
    echo -n .
   done
@@ -15,8 +15,8 @@ done
 
 for ID in $(seq 1 3); do
   echo waiting for ${ID}/3 core-dns pods
-  until [[ $( kubectl -n kube-system get po -l k8s-app=kube-dns --no-headers=true | grep Running | grep 1/1 | wc -l ) -ge "${ID}" ]]; do
-   sleep 0.5
+  until [[ $( kubectl -n kube-system get po -l k8s-app=kube-dns --no-headers=true --ignore-not-found=true | grep Running | grep 1/1 | wc -l ) -ge "${ID}" ]]; do
+   sleep 1
    echo -n .
   done
   echo done
@@ -24,8 +24,8 @@ done
 
 for ID in $(seq 1 ${NODES_COUNT}); do
   echo waiting for ${ID}/${NODES_COUNT} node-local-dns pods
-  until [[ $( kubectl -n kube-system get po -l k8s-app=node-local-dns --no-headers=true | grep Running | grep 1/1 | wc -l ) -ge "${ID}" ]]; do
-   sleep 0.5
+  until [[ $( kubectl -n kube-system get po -l k8s-app=node-local-dns --no-headers=true --ignore-not-found=true | grep Running | grep 1/1 | wc -l ) -ge "${ID}" ]]; do
+   sleep 1
    echo -n .
   done
   echo done
